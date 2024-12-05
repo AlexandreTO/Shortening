b37cabcfb4f4ce74d44e853a5ab6c77848e6a613
Alexandre TO <alexandreto1996@gmail.com>
2024-12-05 10:41:44 +0100

    entity

---
+++ src/Entity/Url.php
@@ -0,0 +1,67 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Entity;
+
+use App\Repository\UrlRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: UrlRepository::class)]
+class Url
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $originalUrl = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $shortenedUrl = null;
+
+    #[ORM\Column]
+    private ?\DateTimeImmutable $createdAt = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getOriginalUrl(): ?string
+    {
+        return $this->originalUrl;
+    }
+
+    public function setOriginalUrl(string $originalUrl): static
+    {
+        $this->originalUrl = $originalUrl;
+
+        return $this;
+    }
+
+    public function getShortenedUrl(): ?string
+    {
+        return $this->shortenedUrl;
+    }
+
+    public function setShortenedUrl(string $shortenedUrl): static
+    {
+        $this->shortenedUrl = $shortenedUrl;
+
+        return $this;
+    }
+
+    public function getCreatedAt(): ?\DateTimeImmutable
+    {
+        return $this->createdAt;
+    }
+
+    public function setCreatedAt(\DateTimeImmutable $createdAt): static
+    {
+        $this->createdAt = $createdAt;
+
+        return $this;
+    }
+}
